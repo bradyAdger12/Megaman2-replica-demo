9d0d402f6f1acb6e4f70ddb5ef66437dc00827f6
admin <joshua.fry@western.edu>
2018-11-13 09:44:32 -0700

    Controller

---
+++ cFiles/Controller.h
@@ -0,0 +1,24 @@
+//
+//  Controller.h
+//  GameFinal
+//
+//  Created by Jfry on 11/13/18.
+//
+
+#ifndef Controller_h
+#define Controller_h
+#include "ofMain.h"
+#include <stdio.h>
+#include <string>
+class Controller{
+public:
+    Controller(string port, int baud);
+    void update();
+    void setup();
+    char getI();
+private:
+    ofSerial input;
+    char i;
+};
+#endif /* Controller_h */
+
